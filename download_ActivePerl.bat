303613889ee8d3abc96c0f0809e0fd161b0d8657
Marcus Ilgner <marcus@doo.net>
2013-10-14 13:34:00 +0200

    Bundle tools to download dependencies on Windows

--- download_ActivePerl.bat
+++ download_ActivePerl.bat
@@ -1,3 +1,4 @@
+SET PATH="%~dp0util";%PATH%
 curl -O -L http://downloads.activestate.com/ActivePerl/releases/5.18.1.1800/ActivePerl-5.18.1.1800-MSWin32-x86-64int-297570.zip
 unzip ActivePerl-5.18.1.1800-MSWin32-x86-64int-297570.zip
 del ActivePerl-5.18.1.1800-MSWin32-x86-64int-297570.zip
--- download_nasm.bat
+++ download_nasm.bat
@@ -1,3 +1,4 @@
+SET PATH="%~dp0util";%PATH%
 curl -O -L http://www.nasm.us/pub/nasm/releasebuilds/2.11rc1/win32/nasm-2.11rc1-win32.zip
 unzip nasm-2.11rc1-win32.zip
 del nasm-2.11rc1-win32.zip
