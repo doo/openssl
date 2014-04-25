4a4958b4835cb2155462c02869b18653f94da51a
Marcus Ilgner <marcus@doo.net>
2014-04-25 16:11:46 +0200

    Make environment modifications in buildscripts local

--- build_win64.bat
+++ build_win64.bat
@@ -1,3 +1,5 @@
+SETLOCAL EnableDelayedExpansion EnableExtensions
+
 SET PATH="%~dp0ActivePerl-5.18.1.1800-MSWin32-x86-64int-297570\perl\bin";"%~dp0nasm-2.11rc1";%PATH%
 IF NOT EXIST ActivePerl-* call download_ActivePerl.bat
 IF NOT EXIST nasm-* call download_nasm.bat
@@ -13,5 +15,7 @@ nmake -f ms\ntdll.mak
 mkdir x64
 copy out32dll\libeay32.* x64\
 copy out32dll\ssleay32.* x64\
+
+ENDLOCAL
 
 EXIT /B 0
